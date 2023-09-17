442644afa0dc401372162c1668ebe2dacba34d88
GrassToucher12 <ttheepicguy@gmail.com>
2023-09-17 13:28:11 +0300

    HowMany.java finished.

--- src/ru/mirea/kvbo0422/task2/HowMany.java
+++ src/ru/mirea/kvbo0422/task2/HowMany.java
@@ -2,15 +2,22 @@ package ru.mirea.kvbo0422.task2;
 
 import java.util.Scanner;
 public class HowMany {
+
     public static void main(String[] args) {
         Scanner sc  = new Scanner(System.in);
-        System.out.printf("Enter your string ");
+        System.out.print("Enter your string ");
         String user_string = sc.nextLine();
+        int k = 1;
         int str_len=user_string.length();
+        char[] chars = new char[str_len];
         for( int i=0; i<str_len; i++) {
-            if (user_string(i)==" "){
-
+            chars[i] = user_string.charAt(i);
+        }
+        for( int i=0; i<str_len; i++) {
+            if ( chars[i] == ' ' ) {
+                k+=1;
             }
         }
+        System.out.println("Number of words in your string is: "+ k);
     }
 }
